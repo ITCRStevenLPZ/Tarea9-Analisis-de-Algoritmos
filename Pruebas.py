7dc119cef15fad4a026d0a6422b52059fa4f962c
unknown <esquivellopezronald@hotmail.com>
2021-10-06 23:56:32 -0600

    [+]Se agrega la asignacion migrada

---
+++ Pruebas.py
@@ -0,0 +1,74 @@
+def merge(arr, l, m, r): ##obtenido de https://www.geeksforgeeks.org/python-program-for-merge-sort/ y modificado para que funcione con las tuplas
+    n1 = m - l + 1
+    n2 = r- m 
+  
+    # create temp arrays 
+    L = [0] * (n1) 
+    R = [0] * (n2) 
+  
+    # Copy data to temp arrays L[] and R[] 
+    for i in range(0 , n1): 
+        L[i] = arr[l + i] 
+  
+    for j in range(0 , n2): 
+        R[j] = arr[m + 1 + j] 
+  
+    # Merge the temp arrays back into arr[l..r] 
+    i = 0     # Initial index of first subarray 
+    j = 0     # Initial index of second subarray 
+    k = l     # Initial index of merged subarray 
+  
+    while i < n1 and j < n2 : 
+        if L[i][1] <= R[j][1]: 
+            arr[k] = L[i] 
+            i += 1
+        else: 
+            arr[k] = R[j] 
+            j += 1
+        k += 1
+  
+    # Copy the remaining elements of L[], if there 
+    # are any 
+    while i < n1: 
+        arr[k] = L[i] 
+        i += 1
+        k += 1
+  
+    # Copy the remaining elements of R[], if there 
+    # are any 
+    while j < n2: 
+        arr[k] = R[j] 
+        j += 1
+        k += 1
+  
+# l is for left index and r is right index of the 
+# sub-array of arr to be sorted 
+def mergeSort(arr,l,r): ##se usara mergeSort, ya que son cantidades pequenas de datos en la lista y cumple con nlogn
+    if l < r: 
+  
+        # Same as (l+r)//2, but avoids overflow for 
+        # large l and h 
+        m = (l+(r-1))//2
+  
+        # Sort first and second halves 
+        mergeSort(arr, l, m) 
+        mergeSort(arr, m+1, r) 
+        merge(arr, l, m, r) 
+
+
+As = [('archivo1', 10), ('archivo2', 5), ('archivo3', 3),
+              ('archivo4', 8), ('archivo5', 9), ('archivo6', 1)]
+#mergeSort(As,0,len(As)-1)
+#print(As)
+
+def maximizar(As, D):
+	mergeSort(As,0,len(As)-1)
+	total = 0
+	Sol = []
+	for i in As:
+		total+=i[1]
+		if total<=D:
+			Sol.append(i)
+	return Sol
+
+print(maximizar(As,10))
